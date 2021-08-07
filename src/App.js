0d3a3ec6adec9f74cc29988699688808017156cb
Eduardo Perez <edrdo.prz@gmail.com>
2021-08-07 17:50:55 -0400

    create naive store solution using `createContext` + `useState`
    - usually this would be handled with something like redux and a more robust react-only solution using context and `useReducer`, but I haven't used React professionally in over two years. i felt this was a happy medium that shows i am somewhat familiar with modern react and thinking about solutions for "global" state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { React } from "react";
+import { React, useEffect, useState } from "react";
 import styled from "styled-components";
 
 import { getColor } from "./_starter/theme/theme";
 import { LeftColumn } from "./components/LeftColumn";
 import { RightColumn } from "./components/RightColumn";
 import { PersonTabbedPanel } from "./components/PersonTabbedPanel";
+import { PersonContext } from "./contexts/person";
 
 const Subheader = styled.div`
   background-color: white;
-  box-shadow: inset 0px -1px 0px ${getColor('greyLightest')};
+  box-shadow: inset 0px -1px 0px ${getColor("greyLightest")};
   display: flex;
   font-size: 20px;
   font-weight: 500;
@@ -22,8 +23,43 @@ const ColumnsContainer = styled.div`
 `;
 
 export const App = ({ children }) => {
+  const [person, setPerson] = useState({
+    details: {},
+    activities: [],
+    upcomingActivities: [],
+  });
+
+  useEffect(() => {
+    fetch("https://ui-offline-exercise.s3.amazonaws.com/data/people.json")
+      .then((res) => res.json())
+      .then(({ activities, upcoming_activities, ...details }) => {
+        setPerson((prev) => ({
+          ...prev,
+          details: {
+            ...details,
+            twitter_url: details.twitter_handle
+              ? `https://twitter.com/${details.twitter_handle}`
+              : null,
+          },
+        }));
+
+        return Promise.all([
+          fetch(activities._href)
+            .then((res) => res.json())
+            .then((res) => res?.data),
+          fetch(upcoming_activities._href)
+            .then((res) => res.json())
+            .then((res) => res?.data),
+        ]);
+      })
+      .then(([activities, upcomingActivities]) => {
+        setPerson((prev) => ({ ...prev, activities, upcomingActivities }));
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <>
+    <PersonContext.Provider value={person}>
       <Subheader>People</Subheader>
       <ColumnsContainer>
         <LeftColumn />
@@ -32,6 +68,6 @@ export const App = ({ children }) => {
 
         <RightColumn />
       </ColumnsContainer>
-    </>
+    </PersonContext.Provider>
   );
 };
