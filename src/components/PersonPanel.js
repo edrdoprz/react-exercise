7ef72bed7ea65de909dace8ae032e3e35cc6ea9d
Eduardo Perez <edrdo.prz@gmail.com>
2021-08-07 17:53:03 -0400

    create `PersonPanelDetails`, which fills out the rest of the person details panel on the left
    - use context to get person details from naive store

--- src/components/PersonPanel.js
+++ src/components/PersonPanel.js
@@ -1,11 +1,10 @@
 import { React } from "react";
-// import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import { getColor } from "../_starter/theme/theme";
 import { IconButton } from "./IconButton";
 import { Panel } from "./Panel";
 import { PanelHeading } from "./PanelHeading";
+import { PersonPanelDetails } from "./PersonPanelDetails";
 
 const StyledPanelHeading = styled(PanelHeading)`
   padding: 8px 15px;
@@ -13,22 +12,25 @@ const StyledPanelHeading = styled(PanelHeading)`
   align-items: center;
 `;
 
-export const PersonPanel = ({ children }) => {
+export const PersonPanel = () => {
   return (
     <Panel>
       <StyledPanelHeading>
-        <IconButton icon="StarOutlined" iconColor="orange" iconHeight="17px" iconWidth="17px" onClick={() => console.log('star clicked')} />
-        <IconButton icon="EllipsisHorizontal" iconHeight="17px" iconWidth="17px" />
+        <IconButton
+          icon="StarOutlined"
+          iconColor="orange"
+          iconHeight="17px"
+          iconWidth="17px"
+          onClick={() => console.log("star clicked")}
+        />
+        <IconButton
+          icon="EllipsisHorizontal"
+          iconHeight="17px"
+          iconWidth="17px"
+        />
       </StyledPanelHeading>
-      <div>Panel</div>
+
+      <PersonPanelDetails />
     </Panel>
   );
 };
-
-// Panel.defaultProps = {
-//   minHeight: 100,
-// };
-
-// Panel.propTypes = {
-//   minHeight: PropTypes.number,
-// };
